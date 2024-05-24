7d91a608184e2d5354f7cde5c0413a3f65cb04a1
Dalikon <dalik.pan@seznam.cz>
2024-05-24 19:38:50 +0200

    Install and set xfce as the DE

---
+++ run.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+# Stop unpredictible behavior
+set -o errexit # Exit on most errors
+set -o nounset # Disallow expansion of unset variables
+set -o pipefail # Use last non-zero exit code in a pipeline
+
+# Define constants
+readonly PROJECTS_DIR=DalaDesktopAnsible
+readonly PLAYBOOK_REPO="https://gitlab.com/radek-sprta/ansible-personal.git"
+readonly PLAYBOOK=setup.yml
+
+function main() {
+  # Install Git and Ansible
+  sudo dnf install -y git ansible
+  # Clone the Ansible playbook and run it cd "${HOME}"
+  mkdir -p "${PROJECTS_DIR}"
+  ansible-pull -U "${PLAYBOOK_REPO}" -i localhost, "${PLAYBOOK}" --ask-become-pass
+}
+
+# Run the script
+main "$@"
